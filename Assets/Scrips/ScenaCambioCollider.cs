5a86316e54fb77f1628b5698c71fd8668393567c
SebastianEsatupinan <124362855+SebastianEsatupinan@users.noreply.github.com>
2023-10-26 21:29:26 -0500

    Guardado de Seguridad
    No se porque pero me lo pide.

--- Assets/Scrips/ScenaCambioCollider.cs
+++ Assets/Scrips/ScenaCambioCollider.cs
@@ -1,17 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// David Alejandro Perez
 public class ScenaCambioCollider : MonoBehaviour
 {
-    /// <summary>
-    /// Método llamado cuando un objeto entra en el colisionador de este objeto.
-    /// Si el objeto que entra es el jugador, carga la escena con el índice 2.
-    /// </summary>
-    /// <param name="collision">El colisionador del objeto que entra en contacto con este objeto.</param>
+    [SerializeField] private string nombreDeLaEscenaACargar; // Esta variable será visible en el Inspector y te permitirá seleccionar la escena a cargar.
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Verifica si el objeto que entra es el jugador
@@ -20,8 +14,8 @@ public class ScenaCambioCollider : MonoBehaviour
             // Muestra un mensaje de depuración en la consola indicando que saliste del nivel
             Debug.Log("Saliste del nivel");
 
-            // Carga la escena con el índice 2
-            SceneManager.LoadScene(2);
+            // Carga la escena con el nombre especificado en el Inspector
+            SceneManager.LoadScene(nombreDeLaEscenaACargar);
         }
     }
 }
